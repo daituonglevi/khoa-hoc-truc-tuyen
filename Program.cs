86e27af7d04c97a3d3a817bfefc3da9f2d1ab18a
Thành Nguyễn <22110198@st.vju.ac.vn>
2026-03-26 21:29:50 -0700

    Configure Azure deploy workflow

--- Program.cs
+++ Program.cs
@@ -103,6 +103,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     try
     {
+        var dbContext = services.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.EnsureCreatedAsync();
         await SeedData.Initialize(services);
     }
     catch (Exception ex)
